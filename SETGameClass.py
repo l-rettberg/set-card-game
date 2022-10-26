83f3f61b30e409240f72bdb5e0bcdc4e14127ff4
Mitchell Faulk <mitchellmfaulk@gmail.com>
2022-10-26 09:12:57 -0400

    Add is_over method

--- SETGameClass.py
+++ SETGameClass.py
@@ -77,6 +77,9 @@ class SETGame:
 
 		self.discard.append(SET)
 
+	def is_over(self):
+		return not (self.selectedposition or self.deck)
+
 	def __iter__(self):
 		self.shuffle()
 		return self
@@ -137,4 +140,5 @@ def simulate(num_games=1000, dimension=None, table_size=None, file_name=None):
 	else:
 		df.to_csv(file_name, index=False)
 
-		
+
+
